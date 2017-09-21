8b25565238436c1a704babc2ac6604d2d5fd0e86
David Henrique <David Henrique>
2017-09-21 07:19:13 -0300

    REferencia pronta, falta testar

--- models/address.js
+++ models/address.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
+const users = require('./users')
 
 const AddressSchema = mongoose.Schema({
     rua: {
@@ -28,6 +29,18 @@ const AddressSchema = mongoose.Schema({
           type:Date,
           default: Date.now, 
           required:true
-    }
+    },
+
+     user: { type: Schema.Types.ObjectId, ref: 'User' }
   });
+   
+
   const Address = module.exports = mongoose.model('Address', AddressSchema);
+
+
+  module.exports.addAdress = function(addAdress, callback){ 
+        if(err) throw err;
+        
+        addAdress.save(callback);
+     
+  }
